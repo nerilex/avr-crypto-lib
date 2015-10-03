8631c5961e22704b8dfd4d74d979e10aa8221063
bg <bg@nerilex.org>
2015-10-03 16:53:32 +0200

    splitting pi16cipher in core and aux part

--- mkfiles/picipher_c.mk
+++ mkfiles/picipher_c.mk
@@ -6,7 +6,7 @@ AEAD_CIPHERS += $(ALGO_NAME)
 
 $(ALGO_NAME)_DIR      := pi-cipher/
 $(ALGO_NAME)_INCDIR   := arcfour/
-$(ALGO_NAME)_OBJ      := pi16cipher.o pi16cipher-asm.o pi32cipher.o pi64cipher.o
+$(ALGO_NAME)_OBJ      := pi16cipher.o pi16cipher-asm.o pi16cipher-aux-asm.o pi32cipher.o pi64cipher.o
 $(ALGO_NAME)_TESTBIN  := main-picipher-test.o arcfour-asm.o $(CLI_STD) performance_test.o
 $(ALGO_NAME)_NESSIE_TEST      := test nessie
 $(ALGO_NAME)_PERFORMANCE_TEST := performance
